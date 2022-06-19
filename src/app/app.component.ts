431f9eade5f6cd71f1a29f0e06d1836d708f422c
charles331 <meunier_charles@live.fr>
2022-06-19 10:28:06 +0200

    Composant base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-lists';
+import { Pokemon } from './pokemon';
+
+//https://www.alexandria-library.co/ressources-angular/
+
 
 @Component({
   selector: 'app-root',
-  template:`<h1>Welcome to {{title}}!</h1>
-  <br>My name is {{name}}`
+  template:`<h1>Liste de Pokémons:</h1>
+  <br>`
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
-  name ='Charles Meunier'
+
+export class AppComponent implements OnInit{
+  //pokemonsList = ['Bulbizarre','Salamèche','Carapuce'];
+  pokemonsList:Pokemon[] = POKEMONS;
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    console.table(this.pokemonsList)
+    this.selectPokemon(this.pokemonsList[10]);
+
+  }
+
+selectPokemon(pokemon:Pokemon){
+  // JS ES6 angular backtick string variable
+  console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`); 
+  // JS ES5 old version
+  console.log('Vous avez cliqué sur le pokémon ' + pokemon.name);
+}
+
 }
